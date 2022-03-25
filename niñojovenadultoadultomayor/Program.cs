ff183585b25deb3db8ef0a52df084aba3da81327
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:30:28 -0700

    Agregar archivos de proyecto.

---
+++ niñojovenadultoadultomayor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace niñojovenadultoadultomayor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu edad");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad <= 12 && edad >=1)
+            {
+                Console.WriteLine("Eres un niño");
+            }
+            else if (edad >= 13 && edad <= 15)
+            {
+                Console.WriteLine("Eres un adolescente");
+            }
+            else if (edad >= 16 && edad <= 17)
+            {
+                Console.WriteLine("Eres un joven");
+            }
+            else if (edad >= 18 && edad <= 59)
+            {
+                Console.WriteLine("Eres un adulto");
+            }
+            else if (edad >= 60)
+            {
+                Console.WriteLine("Eres un adulto tercera edad");
+            }
+        }
+    }
+}
